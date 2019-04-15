aabe59db0d7ab8b2649488ba2ace7505fefb1769
surrealsury <36212513+surrealsury@users.noreply.github.com>
2019-04-14 23:00:17 -0400

    Update Mouse.java
    Mouse methods and constructor added

--- Final Project/Mouse.java
+++ Final Project/Mouse.java
@@ -6,5 +6,18 @@ package Project;
  * Public class Mouse contains the behaviors and attributes of Mice, the primary consumables in the Harrier game.
  */
 public class Mouse extends Animal {
+	int size;
+	
+	public Mouse() {
+		super();
+		setXWidth(5);
+		setYWidth(5);
+		size = 1;
+	}
+
+	public int getSize() { return size; }
+
+	public void setSize(int size) { this.size = size; }
+	
 	
 }
