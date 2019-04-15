ee7fcd5ec63d5fb44713a2f526fb86a2fadbc3b0
surrealsury <36212513+surrealsury@users.noreply.github.com>
2019-04-14 23:03:58 -0400

    Update Fox.java

--- Final Project/Fox.java
+++ Final Project/Fox.java
@@ -6,7 +6,21 @@ package Project;
  * Public class Fox contains the behaviors and attributes of Foxes, the predators in the Harrier game.
  */
 public class Fox extends Animal {
+	int size;
 	
+	public Fox() {
+		super();
+		setXWidth(5);
+		setYWidth(5);
+		size = 1;
+	}
+
+	public int getSize() { return size; }
+
+	public void setSize(int size) { this.size = size; }
+	
+	
+}
 	/*
 	 * public method chase.
 	 * Takes Harrier object and returns nothing.
