0f3f8abad13cedcfee6e94328f2aadce83f6f373
surrealsury <36212513+surrealsury@users.noreply.github.com>
2019-04-21 21:35:17 -0400

    Add files via upload
    Osprey Test for methods

--- Final Project/OspreyTest.java
+++ Final Project/OspreyTest.java
@@ -1,66 +1,46 @@
+package Project;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class OspreyTest {
+class OspreyTest {
+
 
 	@Test
-	void interactFish() {
+	void testDive() {
+		
 		
 		Osprey o = new Osprey();
-		Fish f = new Fish(0, 0, 10);
-		o.setXVel(2);
-		
-		
-		o.interact(f);
-		
-		assertEquals(o.getXVel(), 4);
-		
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void interactSeaweed() {
-		
-		Osprey o = new Osprey();
-		Seaweed s = new Seaweed(2, 2);
-		o.setXVel(2);
-		
-		
-		o.interact(s);
-		
-		assertEquals(o.getXVel(), -2);
-		
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	void interactGoldenFish() {
-		
-		Osprey o = new Osprey();
-		GoldenFish gf = new GoldenFish(0, 0);
-		o.setXVel(2);
-		
-		
-		o.interact(gf);
-		
-		assertEquals(o.getXVel(), 6);
-		
-		fail("Not yet implemented");
-	}
-	
-	
-	@Test
-	void isDiving() {
-		
-		Osprey o = new Osprey();
-		o.setYVel(1);
 		
 		o.dive();
 		
-		assertEquals(o.getYVel(), 2);
+		assertEquals(o.getYVel(), 5);
+		fail("Not yet implemented");
+	}
 
+	@Test
+	void testRise() {
+		
+		Osprey o = new Osprey();
+		
+		o.rise();
+		
+		assertEquals(o.getYVel(), -5);
+		
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		
+		Osprey o = new Osprey();
+		
+		assertEquals(o.toString(), "Osprey @ (0,0)");
+		
+		
+		
 		
 		fail("Not yet implemented");
 	}
