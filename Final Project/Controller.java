2b9b3682a3a19a7774f06862bf3bf6c5c5a7c8f8
Branaffa <37603734+Branaffa@users.noreply.github.com>
2019-05-05 19:34:34 -0400

    P to pause game, ESC to return to menu

--- Final Project/Controller.java
+++ Final Project/Controller.java
@@ -19,15 +19,17 @@ public class Controller implements ActionListener, KeyListener {
 	private OspreyView ov;
 	private HarrierModel hm;
 	private HarrierView hv;
+	private boolean Paused = false; 
 	final static int TICK_TIME = 50;
-	
+
 	public void start() {
 		gs = GameState.TITLE;
 		tv = new TitleView();
 		tv.addListener(this);
 	}
-	
+
 	public void startOsprey() {
+		tv.dispose();
 		gs = GameState.OSPREY;
 		om = new OspreyModel();
 		ov = new OspreyView();
@@ -36,14 +38,25 @@ public class Controller implements ActionListener, KeyListener {
 		TimerTask timerTask = new TimerTask() {
 			@Override
 			public void run() {
-				om.update();
-				ov.update(om.getOsprey(), om.getFish(), om.getSeaweed());
+				if(!Paused) {
+					om.update();
+					ov.update(om.getOsprey(), om.getFish(), om.getSeaweed());
+				}
+				else {
+					try {
+						Thread.sleep(1);
+					}
+					catch(InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 		};
 		timer.schedule(timerTask, 0, TICK_TIME);
 	}
 
 	public void startHarrier() {
+		tv.dispose();
 		gs = GameState.HARRIER;
 		hm = new HarrierModel();
 		hv = new HarrierView();
@@ -52,8 +65,18 @@ public class Controller implements ActionListener, KeyListener {
 		TimerTask timerTask = new TimerTask() {
 			@Override
 			public void run() {
-				hm.update();
-				hv.update(hm.getHarrier(), hm.getFoxes(), hm.getMice(), hm.getTwigs(), hm.getTrees());
+				if(!Paused) {
+					hm.update();
+					hv.update(hm.getHarrier(), hm.getFoxes(), hm.getMice(), hm.getTwigs(), hm.getTrees());
+				}
+				else {
+					try {
+						Thread.sleep(1);
+					}
+					catch(InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 		};
 		timer.schedule(timerTask, 0, TICK_TIME);
@@ -66,12 +89,16 @@ public class Controller implements ActionListener, KeyListener {
 	public void keyPressed(KeyEvent e) {
 		if (gs == GameState.OSPREY) {
 			if (e.getKeyCode() == KeyEvent.VK_SPACE) { om.getOsprey().dive(); }
+			if (e.getKeyCode() == KeyEvent.VK_P) { Paused = !Paused;}
+			if (e.getKeyCode() == KeyEvent.VK_ESCAPE) { gs = GameState.TITLE; ov.frame.dispose(); start();}
 		}
 		else if(gs == GameState.HARRIER) {
 			if (e.getKeyCode() == KeyEvent.VK_UP) { hm.getHarrier().goNorth(); }
 			if (e.getKeyCode() == KeyEvent.VK_DOWN) { hm.getHarrier().goSouth(); }
 			if (e.getKeyCode() == KeyEvent.VK_LEFT) { hm.getHarrier().goWest(); }
 			if (e.getKeyCode() == KeyEvent.VK_RIGHT) { hm.getHarrier().goEast(); }
+			if (e.getKeyCode() == KeyEvent.VK_P) { Paused = !Paused;}
+			if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {gs = GameState.TITLE; hv.frame.dispose(); start();}
 		}
 	}
 
@@ -90,14 +117,14 @@ public class Controller implements ActionListener, KeyListener {
 	 */
 	@Override
 	public void keyTyped(KeyEvent e) {}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton src = (JButton) e.getSource();
 		if (src.equals(tv.getButtonO())) { startOsprey(); }
 		else if (src.equals(tv.getButtonH())) { startHarrier(); }
 	}
-	
+
 	public static void main(String[] args) {
 		Controller c = new Controller();
 		c.start();
