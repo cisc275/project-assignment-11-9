5689dd4e383fb8974223383a12c8c854b89285d2
vbeardsley <48138464+vbeardsley@users.noreply.github.com>
2019-05-06 14:39:25 -0400

    Added fish and seaweed images to game

--- Final Project/OspreyView.java
+++ Final Project/OspreyView.java
@@ -1,6 +1,7 @@
 //Authors: Vincent Beardsley, Suryanash Gupta, Tyler Ballance, Brandon Raffa
 package Project;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.*;
 import javax.swing.*;
 
@@ -9,9 +10,11 @@ public class OspreyView extends GameView{
 	private OspreyHelper helper;
 	final static int X_OFFSET = 100;
 	final static int Y_OFFSET = 100;
+	Image[] images;
 
 	public OspreyView() {
 		super();
+		initializeImages();
 		helper = new OspreyHelper();
 		frame.getContentPane().add(helper);
 		frame.repaint();
@@ -22,6 +25,14 @@ public class OspreyView extends GameView{
 		helper.setFish(fish);
 		helper.setSeaweed(seaweed);
 		frame.repaint();
+	}
+	
+	private void initializeImages() {
+		images = new BufferedImage[4];
+		images[0] = createBufferedImage("OspreyBackground3.png");
+		images[1] = createBufferedImage("OspreyFlying.png");
+		images[2] = createBufferedImage("fish.png");
+		images[3] = createBufferedImage("seaweed.png");
 	}
 	
 	private class OspreyHelper extends JPanel {
@@ -49,22 +60,20 @@ public class OspreyView extends GameView{
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.drawImage(createBufferedImage("OspreyBackground3.png"), 0, 0,TitleView.FRAME_WIDTH, TitleView.FRAME_HEIGHT, null);
-			g.drawImage(createBufferedImage("OspreyFlying.png"), 
+			g.drawImage(images[0], 0, 0,TitleView.FRAME_WIDTH, TitleView.FRAME_HEIGHT, null);
+			g.drawImage(images[1], 
 						X_OFFSET - (int)(osprey.getXWidth() / 2), 
 						(int)osprey.getYPos() + Y_OFFSET - (int)(osprey.getYWidth() / 2), 
 						(int)osprey.getXWidth(), (int)osprey.getYWidth(), this);
 			for(Fish f : fish) {
 				int x = (int)(f.getXPos() - osprey.getXPos()) + X_OFFSET - (int)(f.getXWidth() / 2);
 				int y = (int)f.getYPos() + Y_OFFSET - (int)(f.getYWidth() / 2);
-				g.fillRect(x, y, (int)f.getXWidth(), (int)f.getYWidth());
-				//g.drawImage(createBufferedImage("fish.png"), x, y, (int)f.getXWidth(), (int)f.getYWidth(), this);
+				g.drawImage(images[2], x, y, (int)f.getXWidth(), (int)f.getYWidth(), this);
 			}
 			for(Seaweed s : seaweed) {
 				int x = (int)(s.getXPos() - osprey.getXPos()) + X_OFFSET - (int)(s.getXWidth() / 2);
 				int y = (int)s.getYPos() + Y_OFFSET - (int)(s.getYWidth() / 2);
-				g.fillRect(x, y, (int)s.getXWidth(), (int)s.getYWidth());
-				//g.drawImage(createBufferedImage("seaweed.png"), x, y, (int)s.getXWidth(), (int)s.getYWidth(), this);
+				g.drawImage(images[3], x, y, (int)s.getXWidth(), (int)s.getYWidth(), this);
 			}
 			g.setColor(Color.BLACK);
 			g.fillRect(10, 10, 100, 50);
