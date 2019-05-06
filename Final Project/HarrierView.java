3b16032fd0acb300994c356b91786ac8173357b2
surrealsury <36212513+surrealsury@users.noreply.github.com>
2019-05-05 20:25:08 -0400

    Add files via upload
    Images added to Harrier View:
    Need to edit background so that either image moves along background or drawing static grasses and images so that movement is more easily identified.

--- Final Project/HarrierView.java
+++ Final Project/HarrierView.java
@@ -1,11 +1,16 @@
 //Authors: Vincent Beardsley, Suryanash Gupta, Tyler Ballance, Brandon Raffa
 package Project;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class HarrierView extends GameView {
-
+	
 	private HarrierHelper helper;
 
 	public HarrierView() {
@@ -54,29 +59,66 @@ public class HarrierView extends GameView {
 
 		public Dimension getPreferredSize() { return new Dimension(TitleView.FRAME_WIDTH, TitleView.FRAME_HEIGHT); }
 		
+		
+		
+		//Read image from file and return
+	    private BufferedImage createImage(String file){
+	    	BufferedImage bufferedImage;
+	    	try {
+	    		String path = "src/images/";
+	    		path += file;
+	    		bufferedImage = ImageIO.read(new File(path));
+	    		return bufferedImage;
+	    	} catch (IOException e) {
+	    		e.printStackTrace();
+	    	}
+	    	return null;
+	    }
+	
+	    public Image ImagePanel(String file) {
+	        try {   
+	        	String path = "src/images/";
+	    		path += file;
+	           Image image = ImageIO.read(new File(path));
+	           return image;
+	        } catch (IOException ex) {
+	             // handle exception...
+	        }
+			return null;
+	     }
+		
+		
+		
+		
+		
+		
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.fillRect(TitleView.FRAME_WIDTH / 2, TitleView.FRAME_HEIGHT / 2, (int)harrier.getXWidth(), (int)harrier.getYWidth());
+			g.drawImage(ImagePanel("HarrierBackground.png"), 0, 0,TitleView.FRAME_WIDTH, TitleView.FRAME_HEIGHT, null);
+			g.drawImage(ImagePanel("Harrier Overtop.png"), TitleView.FRAME_WIDTH / 2, TitleView.FRAME_HEIGHT / 2, (int)harrier.getXWidth(), (int)harrier.getYWidth(), this);
 			for(Fox f : foxes) {
 				int x = (int)(f.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2;
 				int y = (int)(f.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2;
-				g.fillRect(x, y, (int)f.getXWidth(), (int)f.getYWidth());
+				g.drawImage(ImagePanel("fox.png"), x, y, (int)f.getXWidth(), (int)f.getYWidth(), this);
+				
+				
+				
 			}
 			for(Mouse m : mice) {
 				int x = (int)(m.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2;
 				int y = (int)(m.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2;
-				g.fillRect(x, y, (int)m.getXWidth(), (int)m.getYWidth());
+				g.drawImage(ImagePanel("mouse.png"), x, y, (int)m.getXWidth(), (int)m.getYWidth(), this);
 			}
 			for(Twig tw : twigs) {
 				int x = (int)(tw.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2;
 				int y = (int)(tw.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2;
-				g.fillRect(x, y, (int)tw.getXWidth(), (int)tw.getYWidth());
+				g.drawImage(ImagePanel("twig.png"), x, y, (int)tw.getXWidth(), (int)tw.getYWidth(), this);
 			}
 			for(Tree tr : trees) {
 				int x = (int)(tr.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - 30;
 				int y = (int)(tr.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - 30;
-				g.fillRect(x, y, (int)tr.getXWidth(), (int)tr.getYWidth());
+				g.drawImage(ImagePanel("Tree.png"), x, y, (int)tr.getXWidth(), (int)tr.getYWidth(), this);
 			}
 		}
 		
