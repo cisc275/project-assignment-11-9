338706a2eaa8756e6bdeb856c903e8ca63e06073
Branaffa <37603734+Branaffa@users.noreply.github.com>
2019-05-19 20:55:09 -0400

    views for the game and leaderboard

--- Project Beta/HarrierView.java
+++ Project Beta/HarrierView.java
@@ -1,25 +1,34 @@
-<<<<<<< HEAD
 //Authors: Vincent Beardsley, Suryanash Gupta, Tyler Ballance, Brandon Raffa
 package Project;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.util.*;
 import java.awt.image.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.swing.*;
 
 public class HarrierView extends GameView {
 
 	private Harrier harrier;
+	private String highScore;
 	private ArrayList<Fox> foxes;
 	private ArrayList<Mouse> mice;
 	private ArrayList<Twig> twigs;
 	private ArrayList<Tree> trees;
+	private FileReader readFile = null;
+	private BufferedReader reader = null;
 	private String[] imageEndings = {"East", "North", "Northeast", "Northwest", "South", "Southeast", "Southwest", "West"};
-	private final static double backgroundScalarX = 1600 / (TitleView.FRAME_WIDTH * 3.0);
-	private final static double backgroundScalarY = 1600 / (TitleView.FRAME_HEIGHT * 3.0);
 
 	public HarrierView() {
 		setOpaque(true);
 		setBackground(Color.green);
+		highScore = "";
 		harrier = new Harrier();
 		foxes = new ArrayList<>();
 		mice = new ArrayList<>();
@@ -40,6 +49,74 @@ public class HarrierView extends GameView {
 		this.twigs = twigs;
 		this.trees = trees;
 	}
+	
+	/*
+	 * Reads the file containing harrier highscore and assigns the string highscore
+	 * Takes in no parameters
+	 * Void method, returns nothing
+	 */
+	public String GetHighScore() {
+		//format: Name: 100
+		
+		try {
+			
+			readFile = new FileReader("highscore.dat");//possibly change this for monday to a different file so we can let her put her initials
+			reader = new BufferedReader(readFile);
+			return reader.readLine();
+		}catch(Exception e) {
+			return "Nobody:0";
+		}
+		finally {
+			try {
+				if(reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {  
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/*
+	 * Compares final score with highest score to see if a new highscore is reached
+	 * Takes in no parameters
+	 * Void method, returns nothing
+	 */
+	public void checkScore() {
+		highScore = GetHighScore();
+		if(harrier.getScore() > Integer.parseInt((highScore.split(":")[1]))) {
+			String name = JOptionPane.showInputDialog("Congratulations on setting a new HighScore! Please enter your name.");
+			while(name.length() >= 4 || filterInput(name).equals("no")) {
+				name = JOptionPane.showInputDialog("Name Invalid. Try Again");
+				}
+			highScore = name + ":" + harrier.getScore();
+
+			File scoreFile = new File("highscore.dat");
+			if(!scoreFile.exists()) {
+				try {
+					scoreFile.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			FileWriter writeFile = null;
+			BufferedWriter writer = null;
+			try {
+				writeFile = new FileWriter(scoreFile);
+				writer = new BufferedWriter(writeFile);
+				writer.write(this.highScore);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if(writer != null) {
+						writer.close();
+					}
+				} catch (Exception e) {}
+			}
+		}
+		}
 
 	/*
 	 * private method initializeImages.
@@ -108,189 +185,10 @@ public class HarrierView extends GameView {
 	 */
 	@Override
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.drawImage(images[0], 0, 0,TitleView.FRAME_WIDTH, TitleView.FRAME_HEIGHT,
-					(int)(backgroundScalarX * (harrier.getXPos() - TitleView.FRAME_WIDTH / 2) + 800),
-					(int)(backgroundScalarY * (harrier.getYPos() - TitleView.FRAME_HEIGHT / 2) + 800),
-					(int)(backgroundScalarX * (harrier.getXPos() + TitleView.FRAME_WIDTH / 2) + 800),
-					(int)(backgroundScalarY * (harrier.getYPos() + TitleView.FRAME_HEIGHT / 2) + 800), this);
-		/*g.setColor(Color.RED);
-		g.drawRect(-TitleView.FRAME_WIDTH / 2 - (int)harrier.getXPos(),
-				   -TitleView.FRAME_HEIGHT / 2 - (int)harrier.getYPos(), 
-				   2 * TitleView.FRAME_WIDTH, 
-				   2 * TitleView.FRAME_HEIGHT);
-		g.setColor(Color.BLACK);*/
-		int hx = TitleView.FRAME_WIDTH / 2 - (int)(harrier.getXWidth() / 2);
-		int hy = TitleView.FRAME_HEIGHT / 2 - (int)(harrier.getYWidth() / 2);
-		g.drawImage(animationFrames[harrier.getAnimation() + 4 * directionConverter(harrier.getDirection())], 
-					hx - (int)(harrier.getXWidth() / 2), hy - (int)(harrier.getYWidth() / 2), (int)(harrier.getXWidth() * 2), (int)(harrier.getYWidth() * 2), this);
-		if(isDebug) { g.drawRect(hx, hy, (int)harrier.getXWidth(), (int)harrier.getYWidth()); }
-		for(Twig tw : twigs) {
-			int x = (int)(tw.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(tw.getXWidth() / 2);
-			int y = (int)(tw.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(tw.getYWidth() / 2);
-			g.drawImage(images[25], x , y, (int)tw.getXWidth(), (int)tw.getYWidth(), this);
-			if(isDebug) { g.drawRect(x, y, (int)tw.getXWidth(), (int)tw.getYWidth()); }
-		}
-		for(Mouse m : mice) {
-			int x = (int)(m.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(m.getXWidth() / 2);
-			int y = (int)(m.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(m.getYWidth() / 2);
-			g.drawImage(images[17 + directionConverter(m.getDirection())], x, y, (int)m.getXWidth(), (int)m.getYWidth(), this);
-			if(isDebug) { g.drawRect(x, y, (int)m.getXWidth(), (int)m.getYWidth()); }
-		}
-		for(Fox f : foxes) {
-			int x = (int)(f.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(f.getXWidth() / 2);
-			int y = (int)(f.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(f.getYWidth() / 2);
-			g.drawImage(animationFrames[f.getAnimation() + 32 + 4 * directionConverter(f.getDirection())],
-						x - (int)(f.getXWidth() / 2), y - (int)(f.getYWidth() / 2), (int)(f.getXWidth() * 2), (int)(f.getYWidth() * 2), this);
-			if(isDebug) { g.drawRect(x, y, (int)f.getXWidth(), (int)f.getYWidth()); }
-		}
-		for(Tree tr : trees) {
-			int x = (int)(tr.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(tr.getXWidth() / 2);
-			int y = (int)(tr.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(tr.getYWidth() / 2);
-			g.drawImage(images[26], x - (int)(tr.getXWidth() * .75), y - (int)(tr.getYWidth() / 2), (int)(tr.getXWidth() * 2.5), (int)(tr.getYWidth() * 2), this);
-			if(isDebug) { g.drawRect(x, y, (int)tr.getXWidth(), (int)tr.getYWidth()); }
-		}
-		/*g.drawImage(images[27], 
-						(int) (0 - harrier.getVision()*20), (int) (0 - harrier.getVision()*20),
-						(int) (TitleView.FRAME_WIDTH + harrier.getVision()*20), (int) (TitleView.FRAME_HEIGHT + harrier.getVision()*20),
-						0, 0, images[6].getWidth(this), images[6].getHeight(this), this);*/
-	}
-
-}
-
-=======
-//Authors: Vincent Beardsley, Suryanash Gupta, Tyler Ballance, Brandon Raffa
-package Project;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
-import java.util.*;
-import java.awt.image.*;
-import javax.swing.*;
-
-public class HarrierView extends GameView {
-
-	private Harrier harrier;
-	private ArrayList<Fox> foxes;
-	private ArrayList<Mouse> mice;
-	private ArrayList<Twig> twigs;
-	private ArrayList<Tree> trees;
-	private String[] imageEndings = {"East", "North", "Northeast", "Northwest", "South", "Southeast", "Southwest", "West"};
-
-	public HarrierView() {
-		setOpaque(true);
-		setBackground(Color.green);
-		harrier = new Harrier();
-		foxes = new ArrayList<>();
-		mice = new ArrayList<>();
-		twigs = new ArrayList<>();
-		trees = new ArrayList<>();
-		initializeImages();
-	}
-
-	/*
-	 * public method update.
-	 * Takes Harrier, ArrayList<Fox>, and ArrayList<Mouse>, ArrayList<Twig>, ArrayList<Tree> as parameters and returns nothing.
-	 * Passes the model objects to the helper for drawing.
-	 */
-	public void update(Harrier harrier, ArrayList<Fox> foxes, ArrayList<Mouse> mice, ArrayList<Twig> twigs, ArrayList<Tree> trees) {
-		this.harrier = harrier;
-		this.foxes = foxes;
-		this.mice = mice;
-		this.twigs = twigs;
-		this.trees = trees;
-	}
-
-	/*
-	 * private method initializeImages.
-	 * Takes no parameter and returns nothing.
-	 * Reads the images into the images array.
-	 */
-	private void initializeImages() {
-		images = new BufferedImage[36];
-		images[0] = createBufferedImage("Harrier Background.png");
-		initializeImageSet("Harrier", 1);
-		initializeImageSet("Fox", 9);
-		initializeImageSet("Mouse", 17);
-		images[25] = createBufferedImage("Twig.png");
-		images[26] = createBufferedImage("Tree.png");
-		images[27] = createBufferedImage("Vision.png");
-		images[28] = createBufferedImage("EnemySound.png");
-		images[29] = createBufferedImage("EnemySound.png");
-		images[30] = createBufferedImage("EnemySound.png");
-		images[31] = createBufferedImage("EnemySound.png");
-		AffineTransform tx1 = new AffineTransform();
-		tx1.rotate(Math.PI/2, images[28].getWidth()/2, images[28].getHeight()/2);
-		AffineTransform tx2 = new AffineTransform();
-		tx2.rotate(Math.PI, images[28].getWidth()/2, images[28].getHeight()/2);
-		AffineTransform tx3 = new AffineTransform();
-		tx3.rotate(Math.PI*3/2, images[28].getWidth()/2, images[28].getHeight()/2);
-		AffineTransformOp op1 = new AffineTransformOp(tx1, AffineTransformOp.TYPE_BILINEAR);
-		AffineTransformOp op2 = new AffineTransformOp(tx2, AffineTransformOp.TYPE_BILINEAR);
-		AffineTransformOp op3 = new AffineTransformOp(tx3, AffineTransformOp.TYPE_BILINEAR);
-		images[29] = op1.filter(images[29], null);
-		images[30] = op2.filter(images[30], null);
-		images[31] = op3.filter(images[31], null);
-		images[32] = createBufferedImage("PreySound.png");
-		images[33] = createBufferedImage("PreySound.png");
-		images[34] = createBufferedImage("PreySound.png");
-		images[35] = createBufferedImage("PreySound.png");
-		images[33] = op1.filter(images[33], null);
-		images[34] = op2.filter(images[34], null);
-		images[35] = op3.filter(images[35], null);
-		makeFrames();
-	}
-	
-	/*
-	 * private method initializeImageSet.
-	 * Takes no parameter and returns nothing.
-	 * Reads a set of images into the images array.
-	 */
-	private void initializeImageSet(String fileSet, int offset) {
-		for(int i = 0; i < imageEndings.length; i++) {
-			String fileName = fileSet;
-			fileName += imageEndings[i] + ".png";
-			images[offset + i] = createBufferedImage(fileName);
-		}
-	}
-
-	/*
-	 * private method makeFrames.
-	 * Takes no parameter and returns nothing.
-	 * Splits those images into their respective frames for animation.
-	 */
-	private void makeFrames() {
-		animationFrames = new BufferedImage[64];
-		for(int i = 0; i < 16; i++) {
-			for(int j = 0; j < 4; j++){
-				animationFrames[j + 4*i] = images[1 + i].getSubimage(165*j, 0, 165, 140);
-			}
-		}
-	}
-
-	/*
-	 * public method directionConverter.
-	 * Takes Direction as parameter and returns int.
-	 * Interprets the direction into an index.
-	 */
-	public int directionConverter(Direction d) {
-		if(d == Direction.EAST) { return 0; }
-		else if(d == Direction.NORTH) { return 1; }
-		else if(d == Direction.NORTHEAST) { return 2; }
-		else if(d == Direction.NORTHWEST) { return 3; }
-		else if(d == Direction.SOUTH) { return 4; }
-		else if(d == Direction.SOUTHEAST) { return 5; }
-		else if(d == Direction.SOUTHWEST) { return 6; }
-		else { return 7; }
-	}
-
-	/*
-	 * public method paintComponent.
-	 * Takes Graphics as parameter and returns nothing.
-	 * Draws the images onto the graphics object for display.
-	 */
-	@Override
-	protected void paintComponent(Graphics g) {
+		g.setFont(new Font(Font.SERIF, Font.BOLD, 16));
+		g.drawString("Score: " + harrier.getScore(), 1450, 20);
+		g.drawString("Press P to pause", 1400, 40);
+		g.drawString("Press ESC to return to menu", 1325, 60);
 		Ellipse2D.Double ellipse = new Ellipse2D.Double(TitleView.FRAME_WIDTH / 2 - harrier.getVision(),
 				TitleView.FRAME_HEIGHT / 2 - harrier.getVision(), harrier.getVision() * 2, harrier.getVision()*2);
 		g.setClip(ellipse);
@@ -300,81 +198,17 @@ public class HarrierView extends GameView {
 		int hy = TitleView.FRAME_HEIGHT / 2 - (int)(harrier.getYWidth() / 2);
 		g.drawImage(animationFrames[harrier.getAnimation() + 4 * directionConverter(harrier.getDirection())], hx, hy, (int)harrier.getXWidth(), (int)harrier.getYWidth(), this);
 		if(isDebug) { g.drawRect(hx, hy, (int)harrier.getXWidth(), (int)harrier.getYWidth()); }
-		g.setColor(Color.RED);
 		for(Fox f : foxes) {
 			int x = (int)(f.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(f.getXWidth() / 2);
 			int y = (int)(f.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(f.getYWidth() / 2);
 			g.drawImage(animationFrames[f.getAnimation() + 32 + 4 * directionConverter(f.getDirection())], x, y, (int)f.getXWidth(), (int)f.getYWidth(), this);
 			if(isDebug) { g.drawRect(x, y, (int)f.getXWidth(), (int)f.getYWidth()); }
-			double myRadius = f.radius(harrier.getYPos(), harrier.getXPos());
-			if (myRadius + 10 >= harrier.getVision()) {
-				switch(f.getApproximateDirection(harrier.getYPos(), harrier.getXPos())) {
-				case NORTH:
-					if (harrier.getVision() < TitleView.FRAME_HEIGHT / 2) {
-						g.drawImage(images[29], TitleView.FRAME_WIDTH / 2 - 30, TitleView.FRAME_HEIGHT / 2 - (int) harrier.getVision(), 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 - 30, TitleView.FRAME_HEIGHT / 2 - (int)harrier.getVision(), 20, 20);
-					} else {
-						g.drawImage(images[29], TitleView.FRAME_WIDTH / 2 - 30, 0, 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 - 30, 0, 20, 20);
-					}
-					break;
-				case SOUTH:
-					if (harrier.getVision() < TitleView.FRAME_HEIGHT / 2) {
-						g.drawImage(images[31], TitleView.FRAME_WIDTH / 2 + 10, TitleView.FRAME_HEIGHT / 2 + (int) harrier.getVision() - 30, 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 + 10, TitleView.FRAME_HEIGHT / 2 + (int)harrier.getVision() - 20, 20, 20);
-					} else {
-						g.drawImage(images[31], TitleView.FRAME_WIDTH / 2 + 10, TitleView.FRAME_HEIGHT - 30, 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 + 10, TitleView.FRAME_HEIGHT - 20, 20, 20);
-					}
-					break;
-				case WEST:
-					g.drawImage(images[28], TitleView.FRAME_WIDTH / 2 - (int)harrier.getVision(), TitleView.FRAME_HEIGHT / 2 - 30, 30, 30, this);
-					//g.fillOval(TitleView.FRAME_WIDTH / 2 - (int)harrier.getVision(), TitleView.FRAME_HEIGHT / 2 - 30, 20, 20);
-					break;
-				case EAST:
-					g.drawImage(images[30], TitleView.FRAME_WIDTH / 2 + (int)harrier.getVision() - 30, TitleView.FRAME_HEIGHT / 2 + 10, 30, 30, this);
-					//g.fillOval(TitleView.FRAME_WIDTH / 2 + (int)harrier.getVision() - 20, TitleView.FRAME_HEIGHT / 2 + 10, 20, 20);
-					break;
-				}
-			}
 		}
-		g.setColor(Color.GREEN);
 		for(Mouse m : mice) {
 			int x = (int)(m.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(m.getXWidth() / 2);
 			int y = (int)(m.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(m.getYWidth() / 2);
 			g.drawImage(images[17 + directionConverter(m.getDirection())], x, y, (int)m.getXWidth(), (int)m.getYWidth(), this);
 			if(isDebug) { g.drawRect(x, y, (int)m.getXWidth(), (int)m.getYWidth()); }
-			double myRadius = m.radius(harrier.getYPos(), harrier.getXPos());
-			if (myRadius + 10 >= harrier.getVision()) {
-				switch(m.getApproximateDirection(harrier.getYPos(), harrier.getXPos())) {
-				case NORTH:
-					if (harrier.getVision() < TitleView.FRAME_HEIGHT / 2) {
-						g.drawImage(images[33], TitleView.FRAME_WIDTH / 2 + 10, TitleView.FRAME_HEIGHT / 2 - (int) harrier.getVision(), 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 + 10, TitleView.FRAME_HEIGHT / 2 - (int)harrier.getVision(), 20, 20);
-					} else {
-						g.drawImage(images[33], TitleView.FRAME_WIDTH / 2 + 10, 0, 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 + 10, 0, 20, 20);
-					}
-					break;
-				case SOUTH:
-					if (harrier.getVision() < TitleView.FRAME_HEIGHT / 2) {
-						g.drawImage(images[35], TitleView.FRAME_WIDTH / 2 - 30, TitleView.FRAME_HEIGHT / 2 + (int) harrier.getVision() - 30, 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 - 30, TitleView.FRAME_HEIGHT / 2 + (int)harrier.getVision() - 20, 20, 20);
-					} else {
-						g.drawImage(images[35], TitleView.FRAME_WIDTH / 2 - 30, TitleView.FRAME_HEIGHT - 30, 30, 30, this);
-						//g.fillOval(TitleView.FRAME_WIDTH / 2 - 30, TitleView.FRAME_HEIGHT - 20, 20, 20);
-					}
-					break;
-				case WEST:
-					g.drawImage(images[32], TitleView.FRAME_WIDTH / 2 - (int)harrier.getVision(), TitleView.FRAME_HEIGHT / 2 + 10, 30, 30, this);
-					//g.fillOval(TitleView.FRAME_WIDTH / 2 - (int)harrier.getVision(), TitleView.FRAME_HEIGHT / 2 + 10, 20, 20);
-					break;
-				case EAST:
-					g.drawImage(images[34], TitleView.FRAME_WIDTH / 2 + (int)harrier.getVision() - 30, TitleView.FRAME_HEIGHT / 2 - 30, 30, 30, this);
-					//g.fillOval(TitleView.FRAME_WIDTH / 2 + (int)harrier.getVision() - 20, TitleView.FRAME_HEIGHT / 2 - 30, 20, 20);
-					break;
-				}
-			}
 		}
 		for(Twig tw : twigs) {
 			int x = (int)(tw.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(tw.getXWidth() / 2);
@@ -382,18 +216,18 @@ public class HarrierView extends GameView {
 			g.drawImage(images[25], x, y, (int)tw.getXWidth(), (int)tw.getYWidth(), this);
 			if(isDebug) { g.drawRect(x, y, (int)tw.getXWidth(), (int)tw.getYWidth()); }
 		}
-		g.setColor(Color.RED);
 		for(Tree tr : trees) {
 			int x = (int)(tr.getXPos() - harrier.getXPos()) + TitleView.FRAME_WIDTH / 2 - (int)(tr.getXWidth() / 2);
 			int y = (int)(tr.getYPos() - harrier.getYPos()) + TitleView.FRAME_HEIGHT / 2 - (int)(tr.getYWidth() / 2);
 			g.drawImage(images[26], x, y, (int)tr.getXWidth(), (int)tr.getYWidth(), this);
 			if(isDebug) { g.drawRect(x, y, (int)tr.getXWidth(), (int)tr.getYWidth()); }
 		}
+		
 		/*g.drawImage(images[27], 
 						(int) (0 - harrier.getVision()*20), (int) (0 - harrier.getVision()*20),
 						(int) (TitleView.FRAME_WIDTH + harrier.getVision()*20), (int) (TitleView.FRAME_HEIGHT + harrier.getVision()*20),
 						0, 0, images[6].getWidth(this), images[6].getHeight(this), this);*/
 	}
+	
 
 }
->>>>>>> a50fe777534a312aa19768ecfe732028af715260
--- Project Beta/OspreyView.java
+++ Project Beta/OspreyView.java
@@ -1,8 +1,13 @@
-<<<<<<< HEAD
 //Authors: Vincent Beardsley, Suryanash Gupta, Tyler Ballance, Brandon Raffa
 package Project;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import javax.swing.*;
 
@@ -13,10 +18,16 @@ public class OspreyView extends GameView {
 	private ArrayList<Seaweed> seaweed;
 	final static int X_OFFSET = 100;
 	final static int Y_OFFSET = 100;
+	private FileReader readFile = null;
+	private BufferedReader reader = null;
+	private String fastTime;
+	
+
 
 	public OspreyView() {
 		setOpaque(true);
 		setBackground(Color.blue);
+		fastTime = "";
 		osprey = new Osprey();
 		fish = new ArrayList<>();
 		seaweed = new ArrayList<>();
@@ -72,111 +83,78 @@ public class OspreyView extends GameView {
 		if(d == Direction.EAST) { return 0; }
 		else { return 1; }
 	}
-
+	
 	/*
-	 * public method paintComponent.
-	 * Takes Graphics as parameter and returns nothing.
-	 * Draws the images onto the graphics object for display.
+	 * Reads the file for fastestTime and assigns the string fastTime to that line
+	 * No Parameters
+	 * Void method, returns nothing
 	 */
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.drawImage(images[0], 0, 0,TitleView.FRAME_WIDTH, TitleView.FRAME_HEIGHT, this);
-		int ox = X_OFFSET - (int)(osprey.getXWidth() / 2);
-		int oy = (int)osprey.getYPos() + Y_OFFSET - (int)(osprey.getYWidth() / 2);
-		g.drawImage(animationFrames[osprey.getAnimation()],
-					ox - (int)(osprey.getXWidth() / 2), oy - (int)(osprey.getYWidth() / 2), (int)(osprey.getXWidth() * 2), (int)(osprey.getYWidth() * 2), this);
-		if(isDebug) { g.drawRect(ox, oy, (int)osprey.getXWidth(), (int)osprey.getYWidth()); }
-		for(Fish f : fish) {
-			int x = (int)(f.getXPos() - osprey.getXPos()) + X_OFFSET - (int)(f.getXWidth() / 2);
-			int y = (int)f.getYPos() + Y_OFFSET - (int)(f.getYWidth() / 2);
-			g.drawImage(animationFrames[f.getAnimation() + 4 + 4* directionConverter(f.getDirection())],
-						x - (int)(f.getXWidth()), y - (int)(f.getYWidth() * 1.5), (int)(f.getXWidth() * 3), (int)(f.getYWidth() * 4), this);
-			if(isDebug) { g.drawRect(x, y, (int)f.getXWidth(), (int)f.getYWidth()); }
+	public String getFastTime() {
+		//format: Name: 100
+		
+		try {
+			
+			readFile = new FileReader("fastTime.dat");//possibly change this for monday to a different file so we can let her put her initials
+			reader = new BufferedReader(readFile);
+			return reader.readLine();
+		}catch(Exception e) {
+			return "Nobody:0";
 		}
-		for(Seaweed s : seaweed) {
-			int x = (int)(s.getXPos() - osprey.getXPos()) + X_OFFSET - (int)(s.getXWidth() / 2);
-			int y = (int)s.getYPos() + Y_OFFSET - (int)(s.getYWidth() / 2);
-			g.drawImage(images[4], x - (int)(s.getXWidth() / 8), y - (int)(s.getYWidth() / 8), (int)(s.getXWidth() * 1.25), (int)(s.getYWidth() * 1.25), this);
-			if(isDebug) { g.drawRect(x, y, (int)s.getXWidth(), (int)s.getYWidth()); }
-		}
-	}
-
-}
-=======
-//Authors: Vincent Beardsley, Suryanash Gupta, Tyler Ballance, Brandon Raffa
-package Project;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.util.*;
-import javax.swing.*;
-
-public class OspreyView extends GameView {
-
-	private Osprey osprey;
-	private ArrayList<Fish> fish;
-	private ArrayList<Seaweed> seaweed;
-	final static int X_OFFSET = 100;
-	final static int Y_OFFSET = 100;
-
-	public OspreyView() {
-		setOpaque(true);
-		setBackground(Color.blue);
-		osprey = new Osprey();
-		fish = new ArrayList<>();
-		seaweed = new ArrayList<>();
-		initializeImages();
-	}
-
-	/*
-	 * public method update.
-	 * Takes Osprey, ArrayList<Fish>, and ArrayList<Seaweed> as parameters and returns nothing.
-	 * Passes the model objects to the helper for drawing.
-	 */
-	public void update(Osprey osprey, ArrayList<Fish> fish, ArrayList<Seaweed> seaweed) {
-		this.osprey = osprey;
-		this.fish = fish;
-		this.seaweed = seaweed;
-	}
-
-	/*
-	 * private method initializeImages.
-	 * Takes no parameter and returns nothing.
-	 * Reads the images into the images array.
-	 */
-	private void initializeImages() {
-		images = new BufferedImage[5];
-		images[0] = createBufferedImage("Osprey Background.png");
-		images[1] = createBufferedImage("Osprey.png");
-		images[2] = createBufferedImage("FishEast.png");
-		images[3] = createBufferedImage("FishWest.png");
-		images[4] = createBufferedImage("newseaweed.png");
-		makeFrames();
-	}
-
-	/*
-	 * private method makeFrames.
-	 * Takes no parameter and returns nothing.
-	 * Splits those images into their respective frames for animation.
-	 */
-	private void makeFrames() {
-		animationFrames = new BufferedImage[12];
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 4; j++){
-				animationFrames[j + 4*i] = images[1 + i].getSubimage(165*j, 0, 165, 140);
+		finally {
+			try {
+				if(reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {  
+				e.printStackTrace();
 			}
 		}
 	}
-
+	
 	/*
-	 * public method directionConverter.
-	 * Takes Direction as parameter and returns int.
-	 * Interprets the direction into an index.
+	 * Compares final time with fastest time to see if a new highscore is reached
+	 * Takes in no parameters
+	 * Void method, returns nothing
 	 */
-	public int directionConverter(Direction d) {
-		if(d == Direction.EAST) { return 0; }
-		else { return 1; }
-	}
+	public void checkTime() {
+		fastTime = getFastTime();
+		if(osprey.gameTimer > Integer.parseInt((fastTime.split(":")[1]))) {
+			String name = JOptionPane.showInputDialog("Congratulations on setting a new HighScore! Please enter your initials.");
+			while(name == null || name.length() >= 4 || filterInput(name).equals("no")) {
+			name = JOptionPane.showInputDialog("Name Invalid. Try Again");
+			}
+			fastTime = name.toUpperCase() + ":" + osprey.gameTimer;
+
+			File scoreFile = new File("fastTime.dat");
+			if(!scoreFile.exists()) {
+				try {
+					scoreFile.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			FileWriter writeFile = null;
+			BufferedWriter writer = null;
+			try {
+				writeFile = new FileWriter(scoreFile);
+				writer = new BufferedWriter(writeFile);
+				writer.write(this.fastTime);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if(writer != null) {
+						writer.close();
+					}
+				} catch (Exception e) {}
+			}
+		}
+		}
+	
+
+	
+
 
 	/*
 	 * public method paintComponent.
@@ -203,7 +181,10 @@ public class OspreyView extends GameView {
 			g.drawImage(images[4], x, y, (int)s.getXWidth(), (int)s.getYWidth(), this);
 			if(isDebug) { g.drawRect(x, y, (int)s.getXWidth(), (int)s.getYWidth()); }
 		}
+		g.drawString("Time: " + osprey.gameTimer + "s", 1450, 20);
+		g.drawString("Press P to pause", 1425, 30);
+		g.drawString("Press ESC to return to menu", 1375, 40);
+		//g.drawString("Time "+ osprey.displayMin + ":" + osprey.displaySec + osprey.display2Sec, 1450, 20);
 	}
 
 }
->>>>>>> a50fe777534a312aa19768ecfe732028af715260
